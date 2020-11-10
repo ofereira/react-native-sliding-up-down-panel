07689bd634de705665df473e811f48cf3f4c7348
Jose Aguirre <jose@jaguirre.net>
2020-11-09 19:56:35 -0700

    Move panel using current status and prev status

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
   Platform,
-  StyleSheet,
-  Text,
   View,
   Animated,
   Dimensions,
@@ -12,24 +10,9 @@ import {
 const { width, height } = Dimensions.get('window');
 let sliderPosition = 0;
 
-const styles = StyleSheet.create({
-  headerPanelViewStyle: {
-    width,
-    backgroundColor: '#ff0032',
-    position: 'absolute',
-    alignSelf: 'flex-end',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1
-  },
-});
-
 const TOP = 'TOP';
 const MIDDLE = 'MIDDLE';
 const BOTTOM = 'BOTTOM';
-
-const statuses = [TOP, MIDDLE, BOTTOM];
-
 
 const SlidingPanelIOS = (props) => (
   <Animated.View style={props.panelPosition === 'bottom' ? {bottom: props.heightAnim, flex: 1, position: 'absolute',} : {top: props.heightAnim, flex: 1, position: 'absolute',}}>
@@ -63,6 +46,7 @@ export default class SlidingPanel extends Component {
       heightAnim: new Animated.Value(props.initialHeight),
       panResponder: {},
       status: MIDDLE,
+      previousStatus: BOTTOM,
     };
 
     sliderPosition = props.initialHeight != 0 ? props.initialHeight + props.maxHeight : 0;
@@ -100,87 +84,97 @@ export default class SlidingPanel extends Component {
         }
       },
       onPanResponderRelease: (e, gesture) => {
-        // console.log('status', this.state.status);
-        console.log('sliderPosition', sliderPosition);
         sliderPosition = sliderPosition + a;
 
         if(a !== 0) { 
-          this.props.onDragStop(e, gesture)
-        }
-        
-        if(this.props.allowAnimation) {
-          if(a === 0 || (this.props.panelPosition === 'bottom' ? gesture.vy < -1 : gesture.vy > 1)) {
-            if(sliderPosition < height-this.props.headerLayoutHeight) {
-              sliderPosition = height-this.props.headerLayoutHeight
-              this.props.onAnimationStart();
-              Animated.timing(
-                this.state.heightAnim,
-                {
-                  toValue: Platform.OS === 'android' ?
-                    height-this.props.headerLayoutHeight - 25 :
-                    height-this.props.headerLayoutHeight - this.props.maxHeight,
-                  duration: this.props.AnimationSpeed,
-                  useNativeDriver: false,
-                }
-              ).start(() => this.props.onAnimationStop());
-              // this.setState((prevState) => ({...prevState, status: TOP}));
-              // console.log('TOP');
-            } else {
-              sliderPosition = this.props.maxHeight
-              this.props.onAnimationStart();
-              Animated.timing(
-                this.state.heightAnim,
-                {
-                  toValue: this.props.minHeight,
-                  duration: this.props.AnimationSpeed,
-                  useNativeDriver: false,
-                }
-              ).start(() => this.props.onAnimationStop()); 
-            }
-            // this.setState((prevState) => ({...prevState, status: BOTTOM}));
-            // console.log('BOTTOM');
-          }
-
-          if(this.props.panelPosition === 'bottom' ? gesture.vy > 1 : gesture.vy < -1) {
-            sliderPosition = 0
-            this.props.onAnimationStart();
-            Animated.timing(
-              this.state.heightAnim,
-              {
-                toValue: this.props.minHeight,
-                duration: this.props.AnimationSpeed,
-                useNativeDriver: false,
-              }
-            ).start(() => this.props.onAnimationStop());
-            console.log('undefined');
-          }
+          this.props.onDragStop(e, gesture);
+          return;
         }
 
         switch(true) {
           case (sliderPosition < 180): {
-            console.log('BOTTOM');
-            this.setState((prevState) => ({...prevState, status: BOTTOM}));
+            this.setState((prevState) => ({...prevState, status: MIDDLE, previousStatus: BOTTOM}));
+
+            sliderPosition = this.goToMiddle();
             break;
           }
-          case (sliderPosition >= 180 && sliderPosition <= 700): {
-            console.log('MIDDLE');
-            this.setState((prevState) => ({...prevState, status: MIDDLE}));
+
+          case (sliderPosition >= 180 && sliderPosition <= 600): {
+            if (this.state.previousStatus === TOP) {
+              sliderPosition = this.goToBottom();
+              this.setState((prevState) => ({...prevState, status: BOTTOM, previousStatus: MIDDLE}));
+            } else {
+              sliderPosition = this.goToTop();
+              this.setState((prevState) => ({...prevState, status: TOP, previousStatus: MIDDLE}));
+            }
             break;
           }
-          case (sliderPosition > 700): {
-            console.log('TOP');
-            this.setState((prevState) => ({...prevState, status: TOP}));
+
+          case (sliderPosition > 600): {
+            sliderPosition = this.goToMiddle();
+            this.setState((prevState) => ({...prevState, status: MIDDLE, previousStatus: TOP}));
             break;
           }
         }
 
         if(this.props.sliderLastPosition){
-          // console.log('last slider', this.props.sliderLastPosition);
           this.props.sliderLastPosition(sliderPosition);
         }
       },
     });
   }
+
+  goToMiddle = () => {
+    const sliderPosition = this.props.initialHeight;
+
+    this.props.onAnimationStart();
+    Animated.timing(
+      this.state.heightAnim,
+      {
+        toValue: Platform.OS === 'android' ?
+          this.props.initialHeight :
+          this.props.initialHeight,
+        duration: this.props.AnimationSpeed,
+        useNativeDriver: false,
+      }
+    ).start(() => this.props.onAnimationStop());
+
+    return sliderPosition;
+  };
+
+  goToTop = () => {
+    const sliderPosition = height - this.props.headerLayoutHeight;
+
+    this.props.onAnimationStart();
+    Animated.timing(
+      this.state.heightAnim,
+      {
+        toValue: Platform.OS === 'android' ?
+          height - this.props.headerLayoutHeight - 25 :
+          height - this.props.headerLayoutHeight - this.props.maxHeight,
+        duration: this.props.AnimationSpeed,
+        useNativeDriver: false,
+      }
+    ).start(() => this.props.onAnimationStop());
+
+    return sliderPosition;
+  };
+
+  goToBottom = () => {
+    const sliderPosition = this.props.maxHeight;
+
+    this.props.onAnimationStart();
+    Animated.timing(
+      this.state.heightAnim,
+      {
+        toValue: this.props.minHeight,
+        duration: this.props.AnimationSpeed,
+        useNativeDriver: false,
+      }
+    ).start(() => this.props.onAnimationStop());
+
+    return sliderPosition;
+  };
 
   onRequestClose() {
     sliderPosition = 0
